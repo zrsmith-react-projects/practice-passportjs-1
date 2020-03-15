34d5478bd7bc8e7331a236531acf402410b70694
Zac Smith <zrs3141592@gmail.com>
2020-03-14 19:58:19 -0600

    create login and register

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ app.get('/', (req, res) => {
   res.render('index.ejs', { name: 'Zac'})
 })
 
+app.get('/login', (req, res) => {
+  res.render('login.ejs')
+})
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+
+})
+app.post('/login', (req, res) => {})
+
 
 const PORT =5555
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`))
