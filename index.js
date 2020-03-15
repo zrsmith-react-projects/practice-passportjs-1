8b4b342c3f60ffe23f4e518cfc0e34305cba2a75
Zac Smith <zrs3141592@gmail.com>
2020-03-14 21:24:10 -0600

    control routes based on authentication

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const session = require('express-session')
 
 const initializePassport = require('./passport-config.js')
 initializePassport(passport, 
-  email => users.find(user => user.email === email)
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
 )
 const users = []
 
@@ -26,21 +27,22 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', { name: 'Zac'})
+app.get('/', checkAuthenticated, (req, res) => {
+  console.log('user', req.user.name)
+  res.render('index.ejs')
 })
 
-app.post('/login', passport.authenticate('local', {
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login',
   failureFlash: true
 }))
 
-app.get('/register', (req, res) => {
+app.get('/register', checkNotAuthenticated, (req, res) => {
   res.render('register.ejs')
 })
 
-app.post('/register', async (req, res) => {
+app.post('/register', checkNotAuthenticated, async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
     users.push({
@@ -56,9 +58,23 @@ app.post('/register', async (req, res) => {
   }
   console.log(users)
 })
-app.get('/login', (req, res) => {
+app.get('/login', checkNotAuthenticated, (req, res) => {
   res.render("login.ejs")
 })
+
+function checkAuthenticated (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated (req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
 
 
 const PORT =5555
